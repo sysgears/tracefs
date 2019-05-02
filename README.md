5210e1c3cd313ba7ba03b68f72379e74511937c1
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-05-02 13:39:17 +0300

    Add initial impl

---
+++ README.md
@@ -0,0 +1,25 @@
+## Tracefs
+
+[![npm version](https://badge.fury.io/js/tracefs.svg)](https://badge.fury.io/js/tracefs)
+[![Twitter Follow](https://img.shields.io/twitter/follow/sysgears.svg?style=social)](https://twitter.com/sysgears)
+
+Traces Node fs calls to files with given path substring and logs requests and results into `<tmpdir>/tracefs.log`
+
+## Usage
+
+```bash
+npm install tracefs
+```
+
+```bash
+TRACEFS=/foo node -r tracefs myprog
+```
+
+And then check the output in
+`<tmpdir>/tracefs.log`
+
+## License
+Copyright © 2019 [SysGears (Cyprus) Limited]. This source code is licensed under the [MIT] license.
+
+[MIT]: LICENSE
+[SysGears (Cyprus) Limited]: http://sysgears.com
---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+function traceFsCalls() {
+  const realFs = {};
+  const fsMethods = Object.keys(fs).filter(function (key) {
+    return key[0] === key[0].toLowerCase() && typeof fs[key] === 'function'
+  });
+  fsMethods.forEach(function (method) {
+    realFs[method] = fs[method];
+    fs[method] = traceFsProxy.bind({ method: method });
+  });
+
+  function traceFsProxy() {
+    try {
+      const result = realFs[this.method].apply(fs, arguments);
+      if (arguments.length > 0 && typeof arguments[0] === 'string' && arguments[0].indexOf(process.env.TRACEFS) >= 0 && arguments[0].indexOf('tracefs.log') < 0) {
+        const dumpedResult = this.method === 'watch' || result === undefined ? '' : ' = ' + JSON.stringify(result instanceof Buffer ? result.toString() : result);
+        realFs.appendFileSync.apply(fs, [path.join(os.tmpdir(), 'tracefs.log'), this.method + ' ' + arguments[0] + dumpedResult + '\n']);
+      }
+      return result;
+    } catch (e) {
+      if (arguments.length > 0 && typeof arguments[0] === 'string' && arguments[0].indexOf(process.env.TRACEFS) >= 0 && arguments[0].indexOf('tracefs.log') < 0)
+        realFs.appendFileSync.apply(fs, [path.join(os.tmpdir(), 'tracefs.log'), this.method + ' ' + arguments[0] +  ' = ' + ((e.message.indexOf('ENOENT') >= 0 || e.message.indexOf('ENOTDIR') >= 0) ? e.message : e.stack) + '\n']);
+
+      throw e;
+    }
+  }
+}
+
+if (process.env.TRACEFS) {
+  traceFsCalls();
+}
